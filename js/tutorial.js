dd411d510d8b5f20072e32a07173db08d5cf53ad
xyroth21 <xyroth21@gmail.com>
2021-12-25 21:33:23 +0100

    Add npm and three.js module

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -5,15 +7,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//const controls = new THREE.( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+//camera.position.set( 0, 20, 100 );
+//controls.update();
+const controls = new OrbitControls( camera, renderer.domElement );
 const geometry = new THREE.BoxGeometry();
 
- const material = new THREE.MeshBasicMaterial( { color: 0x2F4B26, wireframe: true } );
+ const material = new THREE.MeshNormalMaterial(  );
 
 
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 function animate() {
 	requestAnimationFrame( animate );
